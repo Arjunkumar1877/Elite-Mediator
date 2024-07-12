709086139e957b8004569302279c1304965f37e5
Arjun kumar vs <arjun.tech177@gmail.com>
2024-07-12 12:26:32 +0530

    push notification test on mobile device

--- Admin/src/main.tsx
+++ Admin/src/main.tsx
@@ -1,31 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistor, store } from './redux/store.ts'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store.ts";
+import { Provider } from "react-redux";
 
 // src/main.ts or src/index.ts
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/firebase-messaging-sw.js')
+if (!("Notification" in window)) {
+  console.log("This browser does not support desktop notification");
+  alert('This browser does not support desktop notification')
+}
+
+alert("heyyyyyyyyyyyyy")
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/firebase-messaging-sw.js")
       .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      }).catch((error) => {
-        console.error('Service Worker registration failed:', error);
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
       });
   });
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-   <PersistGate persistor={persistor}>
-<Provider store={store}>
-<App />
-</Provider>
 
-   </PersistGate>
-  </React.StrictMode>,
-)
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
+  </React.StrictMode>
+);
