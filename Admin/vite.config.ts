96332126ad679210a0793f6ca6f1b431e13d0db3
Arjun kumar vs <arjun.tech177@gmail.com>
2024-07-21 23:44:37 +0530

    vite configuration changed and build

--- Admin/vite.config.ts
+++ Admin/vite.config.ts
@@ -1,22 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   server: {
     proxy: {
-      '/api':{
-        // target: 'http://13.60.104.214',
-        target: 'http://localhost:7000',
-
-        secure: false,
-      },
-      '/user':{
+      '/api': {
         // target: 'http://13.60.104.214',
         target: 'http://localhost:7000',
         secure: false,
       },
-      '/superAdmin':{
+      '/user': {
+        // target: 'http://13.60.104.214',
+        target: 'http://localhost:7000',
+        secure: false,
+      },
+      '/superAdmin': {
         // target: 'http://13.60.104.214',
         target: 'http://localhost:7000',
         secure: false,
@@ -24,5 +22,16 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1500, // Adjust the limit as needed
+  },
 })
-
